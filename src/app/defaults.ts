264179ddce899db5b13c4df550c45fcf61794e24
Maihan Nijat <maihannijat@gmail.com>
2019-07-29 20:05:25 -0400

    default.ts added to hold lookups and defaults for the project.

---
+++ src/app/defaults.ts
@@ -0,0 +1,19 @@
+export const PARTS = [
+    {id: 0, eng: 'Adjective'},
+    {id: 1, eng: 'Adverb'},
+    {id: 2, eng: 'Article'},
+    {id: 3, eng: 'Conjunction'},
+    {id: 4, eng: 'Interjection'},
+    {id: 5, eng: 'Noun'},
+    {id: 6, eng: 'Preposition'},
+    {id: 7, eng: 'Pronoun'},
+    {id: 8, eng: 'Verb'}
+];
+
+export const ORIGINS = [
+    {id: 0, eng: 'Arabic'},
+    {id: 1, eng: 'Dari'},
+    {id: 2, eng: 'English'},
+    {id: 3, eng: 'Pashto'},
+    {id: 4, eng: 'Urdu'}
+];
